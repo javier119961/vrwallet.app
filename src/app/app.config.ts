d3f3a5c029cefd41b4847787e4bc8c02af9b4f84
Javier Vite Ramirez <javier.vite@puebla.gob.mx>
2026-02-20 12:44:29 -0600

    update Angular dependencies and add PrimeNG theme support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
+import Aura from '@primeuix/themes/aura';
+import {providePrimeNG} from "primeng/config";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+
   ],
 };
+
