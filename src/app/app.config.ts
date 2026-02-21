eb5f3c62667b8a97e3dd104449585bbf62c4cbc5
Javier Vite Ramirez <javier.vite@puebla.gob.mx>
2026-02-20 18:04:37 -0600

    update Angular dependencies and add PrimeNG theme support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,28 @@ import { routes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 import Aura from '@primeuix/themes/aura';
 import {providePrimeNG} from "primeng/config";
+import {definePreset} from "@primeuix/themes";
+
+const MyCustomPreset = definePreset(Aura, {
+  semantic: {
+    transitionDuration: '0.2s',
+    disabledOpacity: '0.6',
+    primary: {
+      color: '{zinc.500}', // Esto mapea internamente a las variables --p-primary
+      hoverColor: '{zinc.600}',
+      activeColor: '{zinc.700}'
+    },
+    formField: {
+      borderRadius: 'var(--p-border-radius-md)',
+      focusRing: {
+        width: '0',
+        style: 'none',
+        color: 'transparent'
+      }
+    },
+    // Puedes replicar toda la estructura que pegaste aquí
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,8 +34,15 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     providePrimeNG({
       theme: {
-        preset: Aura
-      }
+        preset: MyCustomPreset,
+        options: {
+          darkModeSelector: '.my-app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind-base, primeng, tailwind-utilities'
+          }
+        }
+      },
     })
   ],
 };
--- src/app/feature/auth/pages/sign-in/sign-in.component.ts
+++ src/app/feature/auth/pages/sign-in/sign-in.component.ts
@@ -3,6 +3,8 @@ import {RouterLink} from "@angular/router";
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {FormErrorLabelComponent} from "@shared/components/form-error-label/form-error-label.component";
 import {AuthService} from "../../services/auth.service";
+import {MessageService} from "primeng/api";
+import { ToastModule} from "primeng/toast";
 
 @Component({
   selector: 'app-sign-in',
@@ -10,7 +12,9 @@ import {AuthService} from "../../services/auth.service";
     RouterLink,
     ReactiveFormsModule,
     FormErrorLabelComponent,
+    ToastModule
   ],
+  providers: [MessageService],
   templateUrl: './sign-in.component.html',
   standalone: true,
   styles: ``
@@ -18,6 +22,7 @@ import {AuthService} from "../../services/auth.service";
 export class SignInComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private messageService = inject(MessageService);
   
   form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -29,7 +34,18 @@ export class SignInComponent {
   });
   
   handleSubmit(): void {
+    console.log("paso")
+
+    this.messageService.add({
+      key: 'confirm',
+      sticky: true,
+      severity: 'custom',
+      summary: 'Uploading your files.',
+      styleClass: 'backdrop-blur-lg rounded-2xl'
+    });
+    
     if (this.form.invalid){
+      
       this.form.markAllAsTouched();
       return;
     }
