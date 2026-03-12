fb080ca25be1a16b644e65bd4166a404e4cd49ab
Javier Vite Ramirez <javier.vite@puebla.gob.mx>
2026-03-12 13:46:39 -0600

    feat: add account summary chart component and integrate into account detail view

---
+++ src/app/feature/account/components/account-summary-chart/account-summary-chart.component.ts
@@ -0,0 +1,52 @@
+import {Component, computed, input} from '@angular/core';
+import {CardComponent} from "@shared/components/card/card.component";
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
+
+@Component({
+  selector: 'vrw-account-summary-chart',
+  imports: [
+    CardComponent,
+    ChartComponent
+  ],
+  templateUrl: './account-summary-chart.component.html',
+})
+export class AccountSummaryChartComponent {
+  color =  input.required<string>();
+
+  chartOpts = computed<Partial<ChartOptions>>(() => {
+    return {
+      series: [
+        {
+          color: this.color(),
+          name: "Amount",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "area"
+      },
+      title: {
+        text: "Account yields"
+      },
+      xaxis: {
+        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+      }
+    }
+  });
+  
+}
--- src/app/feature/account/pages/account-detail/account-detail.component.ts
+++ src/app/feature/account/pages/account-detail/account-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {Component, computed, inject, input} from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
@@ -7,6 +7,9 @@ import { AccountService } from '../../services/account.service';
 import { CardComponent } from '@shared/components/card/card.component';
 import { AccountTransactionItemComponent } from '../../components/account-transaction-item/account-transaction-item.component';
 import {AccountSummaryCardComponent} from "../../components/account-summary-card/account-summary-card.component";
+import {NgApexchartsModule} from "ng-apexcharts";
+import {AccountSummaryChartComponent} from "../../components/account-summary-chart/account-summary-chart.component";
+
 
 @Component({
   selector: 'vrw-account-detail',
@@ -18,10 +21,13 @@ import {AccountSummaryCardComponent} from "../../components/account-summary-card
     ProgressSpinner,
     AccountTransactionItemComponent,
     AccountSummaryCardComponent,
+    NgApexchartsModule,
+    AccountSummaryChartComponent,
   ],
   templateUrl: './account-detail.component.html',
 })
 export default class AccountDetailComponent {
+  
   public id = input.required<string>();
 
   private accountService = inject(AccountService);
@@ -37,6 +43,7 @@ export default class AccountDetailComponent {
   });
 
   account = computed(() => this.accountResource.value());
+  isInitialLoading = computed(() => this.accountResource.isLoading());
   accountStyle = computed(() => {
     const color = this.account()?.color || '#009688';
     return {
@@ -44,5 +51,6 @@ export default class AccountDetailComponent {
       light: `${color}1A`
     };
   });
-  isInitialLoading = computed(() => this.accountResource.isLoading());
+
+  protected readonly statusbar = statusbar;
 }
